4c2859e46d8084bd1b80c6c266a3b1e9e50b9ca4
T-h-omas <37363928+T-h-omas@users.noreply.github.com>
2018-03-22 11:23:08 +0100

    Add files via upload

--- Cl_alpha.m
+++ Cl_alpha.m
@@ -1,72 +1,58 @@
-function [Cla,CL] = Cl_alpha ()
+function [Cla] = Cl_alpha ()
 
-a = load('FTISxprt-20180306_082856');
+%standard atmospher parameters
+T0 = 288.15; %in K
+p0 = 101325; %in Pa
+rho0 = 1.225; %in Kg/m^3
+lambda = -0.0065; %in K/m
+g0 = 9.80665; %in m/s^2
+R = 287.058; %in J/(Kg K)
+gamma = 1.4; %Heat capacity ratio
 
-%time steps for first measurements
-t_1 = 13650;
-t_2 = 19000;
-
-
-
-hp0    =  a.flightdata.Dadc1_alt.data*0.3048 ;      	  % pressure altitude in the stationary flight condition [m]
-alpha  =    degtorad(a.flightdata.vane_AOA.data)  ;        % angle of attack in the stationary flight condition [rad]
-Vc     =  a.flightdata.Dadc1_cas.data*0.514444;        %computed airspeed in teh stationary flight conditions [m/sec]
-
+%aircraft properties
 S      = 30.00;	          % wing area [m^2]
-rho0   = 1.2250;          % air density at sea level [kg/m^3] 
-lambda = -0.0065;         % temperature gradient in ISA [K/m]
-Temp0  = 288.15;          % temperature at sea level in ISA [K]
-R      = 287.05;          % specific gas constant [m^2/sec^2K]
-g      = 9.81;            % [m/sec^2] (gravity constant)
 
 
-[m,~] = mass_and_balance ();
-W = m*g;
-
-[Vre,~] = Vreducedequivalent();
-
-% rho = [];
-% for n = 1:48681
-%     rho_1 = rho0*((1+(lambda*(hp0(n)/Temp0))))^(-((g/(lambda*R))+1));
-%     rho =[rho, rho_1];
-% end
-% rho = rho (:);              % [kg/m^3]  (air density)
-
+%flight test data from stationary measurements CL-CD series 1
+Cas = [250,221,189,162,130,116];
+Cas = Cas * 0.514444;
+TAT = [3.2,1.2,-0.5,-2.5,-3.8,-3.8];
+TAT = TAT + 273.15;
+hp  = [5040,5050,5030,5050,4960,4980];
+alpha = [1.2,1.9,3.2,4.8,7.4,10];
 
 
 CL = [];
-% 
-% for n = 1:48681
-%     CL_1 = 2*W(n)/(rho(n)*Vc(n)^2*S);               % Lift coefficient [ ]
-% 
-%     CL = [CL, CL_1];
-%    
-% end
 
+[weight, ~] = mass_and_balance(); %Mass of a/c in Kg
+W = weight*g0; %Weight of a/c in N
 
-for n = 1:48681
-    CL_1 = 2*W(n)/(rho0*Vre(n)^2*S);               % Lift coefficient [ ]
+%Calculate pressure in Pa
+p = p0*(1+(lambda*hp)/T0).^(-g0/(lambda*R));
 
-    CL = [CL, CL_1];
-   
-end
+%Calculate mach
+M = sqrt(2/(gamma-1)*((1+(p0*p.^-1).*((1+((gamma-1)/(2*gamma))*(rho0/p0)*Cas.^2).^(gamma/(gamma-1))-1)).^((gamma-1)/gamma)-1));
 
-CL = CL (:);
+%Calculate static temperature
+T = TAT.*(1+((gamma-1)/2)*M.^2).^-1;
 
+%Calculate speed of sound
+a = sqrt(gamma*R*T);
 
-CL_2 = CL(t_1:t_2);
-alpha_2 = radtodeg(alpha (t_1:t_2));
+%Calculate true airspeed
+Vt = M.* a;
 
-mdl = fitlm(alpha_2,CL_2,'poly1');
+%Calculate density 
+rho = p.*((R*T).^-1);
+
+W = [W(1)-247*0.453592,W(1)-285*0.453592,W(1)-312*0.453592,W(1)-357*0.453592,W(1)-410*0.453592,W(1)-444*0.453592];
+
+CL = 2*W./(rho.*Vt.^2*S);               % Lift coefficient [ ]
+
+mdl = fitlm(alpha,CL,'linear');
 
 %plot(mdl)
-%xlabel('angle of attack [deg]');
-%ylabel('Cl');
-%title('Cl-alpha');
-CL_linreg = mdl.Fitted;
 
-
-
-Cla   = (CL_linreg(end)-CL_linreg(1))/ (alpha(t_2)-alpha(t_1)) ;            % Slope of CL-alpha curve [ ]
-
-
+coefficients = mdl.Coefficients.Estimate;
+Cla = radtodeg(coefficients(2));
+a_0 = coefficients(1);
