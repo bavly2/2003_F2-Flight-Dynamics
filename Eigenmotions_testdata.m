e11107e00b29f104a4d31f99b0b2d003fc2d7a72
arvanLeeuwen <37363818+arvanLeeuwen@users.noreply.github.com>
2018-03-22 10:52:30 +0100

    Add files via upload

--- Eigenmotions_testdata.m
+++ Eigenmotions_testdata.m
@@ -1,7 +1,7 @@
-a = load('FTISxprt-20180306_082856');
+a = load('FTISxprt-20180320_102524');
 
 t_phugoid = 150; %sec, duration of the eigenmotion
-t_shortp = 7;
+t_shortp = 10;
 t_spiral = 100;
 t_Droll = 20;
 t_DrollYD = 20;
@@ -38,12 +38,12 @@ for i = 1:length(delta_r)-1
     dr = [dr,slope_r];
 end
 
-start_phugoid = 60*60 + 48 ; %min:sec
-start_shortp = 55*60 + 50 ;
-start_Droll = 57*60 + 4 ;
-start_DrollYD = 57*60 + 45 ;
-start_Aroll = 59*60 + 13 ;
-start_spiral = 64*60 + 57 ;
+start_phugoid = 51*60 + 48 ; %min:sec
+start_shortp = 50*60 + 10 ;
+start_Droll = 55*60 + 23 ;
+start_DrollYD = 56*60 + 26 ;
+start_Aroll = 49*60 + 00 ;
+start_spiral = 58*60 + 00 ;
 
 %phugoid plots
 
@@ -60,27 +60,27 @@ V_phugoid = TAS(i_start_phugoid:i_start_phugoid+t_phugoid*10-1);
 aoa_phugoid = aoa(i_start_phugoid:i_start_phugoid+t_phugoid*10-1);
 pitchrate_phugoid = pitchrate(i_start_phugoid:i_start_phugoid+t_phugoid*10-1);
 
-phugoid1 = subplot(2,2,1);
-plot(phugoid1,time_phugoid,pitch_phugoid)
-xlabel(phugoid1,'t [sec]')
-ylabel(phugoid1,'theta [deg]')
-title(phugoid1,'Phugoid')
-
-phugoid2 = subplot(2,2,2);
-plot(phugoid2,time_phugoid,V_phugoid)
-xlabel(phugoid2,'t [sec]')
-ylabel(phugoid2,'V [m/s]')
-
-phugoid3 = subplot(2,2,3);
-plot(phugoid3,time_phugoid,aoa_phugoid)
-xlabel(phugoid3,'t [sec]')
-ylabel(phugoid3,'alpha [deg]')
-
-phugoid4 = subplot(2,2,4);
-plot(phugoid4,time_phugoid,pitchrate_phugoid)
-xlabel(phugoid4,'t [sec]')
-ylabel(phugoid4,'q [deg/s]')
-figure()
+% phugoid1 = subplot(2,2,1);
+% plot(phugoid1,time_phugoid,pitch_phugoid);
+% xlabel(phugoid1,'t [sec]');
+% ylabel(phugoid1,'theta [deg]');
+% title(phugoid1,'Phugoid');
+% 
+% phugoid2 = subplot(2,2,2);
+% plot(phugoid2,time_phugoid,V_phugoid);
+% xlabel(phugoid2,'t [sec]');
+% ylabel(phugoid2,'V [m/s]');
+% 
+% phugoid3 = subplot(2,2,3);
+% plot(phugoid3,time_phugoid,aoa_phugoid);
+% xlabel(phugoid3,'t [sec]');
+% ylabel(phugoid3,'alpha [deg]');
+% 
+% phugoid4 = subplot(2,2,4);
+% plot(phugoid4,time_phugoid,pitchrate_phugoid);
+% xlabel(phugoid4,'t [sec]');
+% ylabel(phugoid4,'q [deg/s]');
+% figure();
 
 %short period plots
 
@@ -99,31 +99,31 @@ V_shortp = TAS(i_start_shortp:i_start_shortp+t_shortp*10-1);
 aoa_shortp = aoa(i_start_shortp:i_start_shortp+t_shortp*10-1);
 pitchrate_shortp = pitchrate(i_start_shortp:i_start_shortp+t_shortp*10-1);
 
-shortp1 = subplot(2,2,1);
-plot(shortp1,time_shortp,pitch_shortp)
-xlabel(shortp1,'t [sec]')
-ylabel(shortp1,'theta [deg]')
-title(shortp1,'Short period')
-
-% shortp2 = subplot(2,1,2);
-% plot(shortp2,time_shortp,de(i_start_shortp:i_start_shortp+t_shortp*10-1))
-
-shortp2 = subplot(2,2,2);
-plot(shortp2,time_shortp,pitchrate_shortp)
-xlabel(shortp2,'t [sec]')
-ylabel(shortp2,'q [deg/s]')
-
-shortp3 = subplot(2,2,3);
-plot(shortp3,time_shortp,aoa_shortp)
-xlabel(shortp3,'t [sec]')
-ylabel(shortp3,'alpha [deg]')
-
-shortp4 = subplot(2,2,4);
-plot(shortp4,time_shortp,V_shortp)
-xlabel(shortp4,'t [sec]')
-ylabel(shortp4,'V [m/s]')
-
-figure()
+% shortp1 = subplot(2,2,1);
+% plot(shortp1,time_shortp,pitch_shortp);
+% xlabel(shortp1,'t [sec]');
+% ylabel(shortp1,'theta [deg]');
+% title(shortp1,'Short period');
+% 
+% % shortp2 = subplot(2,1,2);
+% % plot(shortp2,time_shortp,de(i_start_shortp:i_start_shortp+t_shortp*10-1))
+% 
+% shortp2 = subplot(2,2,2);
+% plot(shortp2,time_shortp,pitchrate_shortp);
+% xlabel(shortp2,'t [sec]');
+% ylabel(shortp2,'q [deg/s]');
+% 
+% shortp3 = subplot(2,2,3);
+% plot(shortp3,time_shortp,aoa_shortp);
+% xlabel(shortp3,'t [sec]');
+% ylabel(shortp3,'alpha [deg]');
+% 
+% shortp4 = subplot(2,2,4);
+% plot(shortp4,time_shortp,V_shortp);
+% xlabel(shortp4,'t [sec]');
+% ylabel(shortp4,'V [m/s]');
+% 
+% figure();
 
 % Dutch roll plots
 
@@ -137,21 +137,21 @@ rollangle_Droll = rollangle(i_start_Droll:i_start_Droll+t_Droll*10-1);
 rollrate_Droll = rollrate(i_start_Droll:i_start_Droll+t_Droll*10-1);
 yawrate_Droll = yawrate(i_start_Droll:i_start_Droll+t_Droll*10-1);
 
-Droll1 = subplot(3,2,1);
-plot(Droll1,time_Droll,rollangle_Droll)
-xlabel(Droll1,'t [sec]')
-ylabel(Droll1,'roll angle [deg]')
-title(Droll1,'Dutch roll')
-
-Droll2 = subplot(3,2,3);
-plot(Droll2,time_Droll,rollrate_Droll)
-xlabel(Droll2,'t [sec]')
-ylabel(Droll2,'roll rate [deg/s]')
-
-Droll3 = subplot(3,2,5);
-plot(Droll3,time_Droll,yawrate_Droll)
-xlabel(Droll3,'t [sec]')
-ylabel(Droll3,'yaw rate [deg/s]')
+% Droll1 = subplot(3,2,1);
+% plot(Droll1,time_Droll,rollangle_Droll);
+% xlabel(Droll1,'t [sec]');
+% ylabel(Droll1,'roll angle [deg]');
+% title(Droll1,'Dutch roll');
+% 
+% Droll2 = subplot(3,2,3);
+% plot(Droll2,time_Droll,rollrate_Droll);
+% xlabel(Droll2,'t [sec]');
+% ylabel(Droll2,'roll rate [deg/s]');
+% 
+% Droll3 = subplot(3,2,5);
+% plot(Droll3,time_Droll,yawrate_Droll);
+% xlabel(Droll3,'t [sec]');
+% ylabel(Droll3,'yaw rate [deg/s]');
 
 % compare with the yaw demper
 
@@ -165,23 +165,23 @@ rollangle_DrollYD = rollangle(i_start_DrollYD:i_start_DrollYD+t_DrollYD*10-1);
 rollrate_DrollYD = rollrate(i_start_DrollYD:i_start_DrollYD+t_DrollYD*10-1);
 yawrate_DrollYD = yawrate(i_start_DrollYD:i_start_DrollYD+t_DrollYD*10-1);
 
-DrollYD1 = subplot(3,2,2);
-plot(DrollYD1,time_DrollYD,rollangle_DrollYD)
-xlabel(DrollYD1,'t [sec]')
-ylabel(DrollYD1,'roll angle [deg]')
-title(DrollYD1,'Dutch roll YD')
-
-DrollYD2 = subplot(3,2,4);
-plot(DrollYD2,time_DrollYD,rollrate_DrollYD)
-xlabel(DrollYD2,'t [sec]')
-ylabel(DrollYD2,'roll rate [deg/s]')
-
-DrollYD3 = subplot(3,2,6);
-plot(DrollYD3,time_DrollYD,yawrate_DrollYD)
-xlabel(DrollYD3,'t [sec]')
-ylabel(DrollYD3,'yaw rate [deg/s]')
-
-figure()
+% DrollYD1 = subplot(3,2,2);
+% plot(DrollYD1,time_DrollYD,rollangle_DrollYD);
+% xlabel(DrollYD1,'t [sec]');
+% ylabel(DrollYD1,'roll angle [deg]');
+% title(DrollYD1,'Dutch roll YD');
+% 
+% DrollYD2 = subplot(3,2,4);
+% plot(DrollYD2,time_DrollYD,rollrate_DrollYD);
+% xlabel(DrollYD2,'t [sec]');
+% ylabel(DrollYD2,'roll rate [deg/s]');
+% 
+% DrollYD3 = subplot(3,2,6);
+% plot(DrollYD3,time_DrollYD,yawrate_DrollYD);
+% xlabel(DrollYD3,'t [sec]');
+% ylabel(DrollYD3,'yaw rate [deg/s]');
+% 
+% figure();
 
 % Aperiodic roll plots
 
@@ -197,23 +197,23 @@ rollangle_Aroll = rollangle(i_start_Aroll:i_start_Aroll+t_Aroll*10-1);
 rollrate_Aroll = rollrate(i_start_Aroll:i_start_Aroll+t_Aroll*10-1);
 yawrate_Aroll = yawrate(i_start_Aroll:i_start_Aroll+t_Aroll*10-1);
 
-Aroll1 = subplot(3,1,1);
-plot(Aroll1,time_Aroll,rollangle_Aroll)
-xlabel(Aroll1,'t [sec]')
-ylabel(Aroll1,'roll angle [deg]')
-title(Aroll1,'Aperiodic roll')
-
-Aroll2 = subplot(3,1,2);
-plot(Aroll2,time_Aroll,rollrate_Aroll)
-xlabel(Aroll2,'t [sec]')
-ylabel(Aroll2,'roll rate [deg/s]')
-
-Aroll3 = subplot(3,1,3);
-plot(Aroll3,time_Aroll,yawrate_Aroll)
-xlabel(Aroll3,'t [sec]')
-ylabel(Aroll3,'yaw rate [deg/s]')
-
-figure()
+% Aroll1 = subplot(3,1,1);
+% plot(Aroll1,time_Aroll,rollangle_Aroll);
+% xlabel(Aroll1,'t [sec]');
+% ylabel(Aroll1,'roll angle [deg]');
+% title(Aroll1,'Aperiodic roll');
+% 
+% Aroll2 = subplot(3,1,2);
+% plot(Aroll2,time_Aroll,rollrate_Aroll);
+% xlabel(Aroll2,'t [sec]');
+% ylabel(Aroll2,'roll rate [deg/s]');
+% 
+% Aroll3 = subplot(3,1,3);
+% plot(Aroll3,time_Aroll,yawrate_Aroll);
+% xlabel(Aroll3,'t [sec]');
+% ylabel(Aroll3,'yaw rate [deg/s]');
+% 
+% figure();
 
 % Spiral plots
 
@@ -229,18 +229,18 @@ rollangle_spiral = rollangle(i_start_spiral:i_start_spiral+t_spiral*10-1);
 rollrate_spiral = rollrate(i_start_spiral:i_start_spiral+t_spiral*10-1);
 yawrate_spiral = yawrate(i_start_spiral:i_start_spiral+t_spiral*10-1);
 
-spiral1 = subplot(3,1,1);
-plot(spiral1,time_spiral,rollangle_spiral)
-xlabel(spiral1,'t [sec]')
-ylabel(spiral1,'roll angle [deg]')
-title(spiral1,'Spiral')
-
-spiral2 = subplot(3,1,2);
-plot(spiral2,time_spiral,rollrate_spiral)
-xlabel(spiral2,'t [sec]')
-ylabel(spiral2,'roll rate [deg/s]')
-
-spiral3 = subplot(3,1,3);
-plot(spiral3,time_spiral,yawrate_spiral)
-xlabel(spiral3,'t [sec]')
-ylabel(spiral3,'yaw rate [deg/s]')
+% spiral1 = subplot(3,1,1);
+% plot(spiral1,time_spiral,rollangle_spiral);
+% xlabel(spiral1,'t [sec]');
+% ylabel(spiral1,'roll angle [deg]');
+% title(spiral1,'Spiral');
+% 
+% spiral2 = subplot(3,1,2);
+% plot(spiral2,time_spiral,rollrate_spiral);
+% xlabel(spiral2,'t [sec]');
+% ylabel(spiral2,'roll rate [deg/s]');
+% 
+% spiral3 = subplot(3,1,3);
+% plot(spiral3,time_spiral,yawrate_spiral);
+% xlabel(spiral3,'t [sec]');
+% ylabel(spiral3,'yaw rate [deg/s]');
