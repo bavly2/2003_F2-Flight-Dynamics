269bb3c46246aa75cf37347b6f66b311b222d639
arvanLeeuwen <37363818+arvanLeeuwen@users.noreply.github.com>
2018-03-26 12:32:44 +0200

    Add files via upload

--- Eigenmotions_testdata.m
+++ Eigenmotions_testdata.m
@@ -1,11 +1,6 @@
 a = load('FTISxprt-20180320_102524');
 
-t_phugoid = 200; %sec, duration of the eigenmotion
-t_shortp = 10;
-t_spiral = 50;
-t_Droll = 20;
-t_DrollYD = 15;
-t_Aroll = 10;
+run('start_index_plots')
 
 time = a.flightdata.time.data ; %sec
 pitchangle = a.flightdata.Ahrs1_Pitch.data ; %deg
@@ -18,47 +13,41 @@ yawrate = a.flightdata.Ahrs1_bYawRate.data; %
 
 %elevator, aileron and rudder deflection
 
-delta_e = a.flightdata.delta_e.data;
-delta_a = a.flightdata.delta_a.data;
-delta_r = a.flightdata.delta_r.data;
-de = [];
-da = [];
-dr = [];
+% delta_e = a.flightdata.delta_e.data;
+% delta_a = a.flightdata.delta_a.data;
+% delta_r = a.flightdata.delta_r.data;
+% % de = [];
+% % da = [];
+% % dr = [];
+% 
+% for i = 1:length(delta_e)-1
+%     slope_e = (delta_e(i+1)-delta_e(i))/0.1;
+%     de = [de,slope_e];
+% end
+% for i = 1:length(delta_a)-1
+%     slope_a = (delta_a(i+1)-delta_a(i))/0.1;
+%     da = [da,slope_a];
+% end
+% for i = 1:length(delta_r)-1
+%     slope_r = (delta_r(i+1)-delta_r(i))/0.1;
+%     dr = [dr,slope_r];
+% end
 
-for i = 1:length(delta_e)-1
-    slope_e = (delta_e(i+1)-delta_e(i))/0.1;
-    de = [de,slope_e];
-end
-for i = 1:length(delta_a)-1
-    slope_a = (delta_a(i+1)-delta_a(i))/0.1;
-    da = [da,slope_a];
-end
-for i = 1:length(delta_r)-1
-    slope_r = (delta_r(i+1)-delta_r(i))/0.1;
-    dr = [dr,slope_r];
-end
 
-start_phugoid = 51*60 + 48 ; %min:sec
-start_shortp = 50*60 + 10 ;
-start_Droll = 55*60 + 23 ;
-start_DrollYD = 56*60 + 26 ;
-start_Aroll = 49*60 + 00 ;
-start_spiral = 58*60 + 00 ;
-
-%phugoid plots
+%%phugoid plots
 
 %check start elevator input
 
-i_phugoid = find(abs(time-start_phugoid)<0.001); %index number where the eigenmotion starts
-range = (i_phugoid-100:i_phugoid+99);
-minde_phugoid = min(de(range));
-i_start_phugoid = find(de==minde_phugoid);
+% i_phugoid = find(abs(time-start_phugoid)<0.001); %index number where the eigenmotion starts
+% range = (i_phugoid-100:i_phugoid+99);
+% minde_phugoid = min(de(range));
+% i_start_phugoid = find(de==minde_phugoid);
 
-time_phugoid = (1:t_phugoid*10)/10;
-pitch_phugoid = pitchangle(i_start_phugoid:i_start_phugoid+t_phugoid*10-1);
-V_phugoid = TAS(5904)-TAS(i_start_phugoid:i_start_phugoid+t_phugoid*10-1);
-aoa_phugoid = aoa(i_start_phugoid:i_start_phugoid+t_phugoid*10-1);
-pitchrate_phugoid = pitchrate(i_start_phugoid:i_start_phugoid+t_phugoid*10-1);
+%time_phugoid = (1:t_phugoid*10)/10;
+pitch_phugoid = pitchangle(i_start_PH:i_start_PH+length_phugoid*10);
+V_phugoid = TAS(i_start_PH:i_start_PH+length_phugoid*10)-TAS(5904);
+aoa_phugoid = aoa(i_start_PH:i_start_PH+length_phugoid*10);
+pitchrate_phugoid = pitchrate(i_start_PH:i_start_PH+length_phugoid*10);
 
 % phugoid1 = subplot(2,2,1);
 % plot(phugoid1,time_phugoid,pitch_phugoid);
@@ -86,18 +75,18 @@ pitchrate_phugoid = pitchrate(i_start_phugoid:i_start_phugoid+t_phugoid*10-1);
 
 %check start elevator input
 
-i_shortp = find(abs(time-start_shortp)<0.001); %index number where the eigenmotion starts
-range = (i_shortp-100:i_shortp+99);
-% plot(time(range),delta_e(range))
-% figure()
-minde_shortp = min(de(range));
-i_start_shortp = find(de==minde_shortp);
+% i_shortp = find(abs(time-start_shortp)<0.001); %index number where the eigenmotion starts
+% range = (i_shortp-100:i_shortp+99);
+% % plot(time(range),delta_e(range))
+% % figure()
+% minde_shortp = min(de(range));
+% i_start_shortp = find(de==minde_shortp);
 
-time_shortp = (1:t_shortp*10)/10;
-pitch_shortp = pitchangle(i_start_shortp:i_start_shortp+t_shortp*10-1);
-V_shortp = TAS(5904)-TAS(i_start_shortp:i_start_shortp+t_shortp*10-1);
-aoa_shortp = aoa(i_start_shortp:i_start_shortp+t_shortp*10-1);
-pitchrate_shortp = pitchrate(i_start_shortp:i_start_shortp+t_shortp*10-1);
+%time_shortp = (1:t_shortp*10)/10;
+pitch_shortp = pitchangle(i_start_SP:i_start_SP+length_shortp*10);
+V_shortp = -TAS(5904)+TAS(i_start_SP:i_start_SP+length_shortp*10);
+aoa_shortp = aoa(i_start_SP:i_start_SP+length_shortp*10);
+pitchrate_shortp = pitchrate(i_start_SP:i_start_SP+length_shortp*10);
 
 % shortp1 = subplot(2,2,1);
 % plot(shortp1,time_shortp,pitch_shortp);
@@ -127,15 +116,15 @@ pitchrate_shortp = pitchrate(i_start_shortp:i_start_shortp+t_shortp*10-1);
 
 % Dutch roll plots
 
-i_Droll = find(abs(time-start_Droll)<0.001); %index number where the eigenmotion starts
-range = (i_Droll-200:i_Droll+99);
-maxdr_Droll = max(dr(range));
-i_start_Droll = find(dr==maxdr_Droll);
+% i_Droll = find(abs(time-start_Droll)<0.001); %index number where the eigenmotion starts
+% range = (i_Droll-200:i_Droll+99);
+% maxdr_Droll = max(dr(range));
+% i_start_Droll = find(dr==maxdr_Droll);
 
-time_Droll = (1:t_Droll*10)/10;
-rollangle_Droll = rollangle(i_start_Droll:i_start_Droll+t_Droll*10-1);
-rollrate_Droll = rollrate(i_start_Droll:i_start_Droll+t_Droll*10-1);
-yawrate_Droll = yawrate(i_start_Droll:i_start_Droll+t_Droll*10-1);
+%time_Droll = (1:t_Droll*10)/10;
+rollangle_Droll = rollangle(i_start_DR:i_start_DR+length_Droll*10);
+rollrate_Droll = rollrate(i_start_DR:i_start_DR+length_Droll*10);
+yawrate_Droll = yawrate(i_start_DR:i_start_DR+length_Droll*10);
 
 % Droll1 = subplot(3,2,1);
 % plot(Droll1,time_Droll,rollangle_Droll);
@@ -155,15 +144,15 @@ yawrate_Droll = yawrate(i_start_Droll:i_start_Droll+t_Droll*10-1);
 
 % compare with the yaw demper
 
-i_DrollYD = find(abs(time-start_DrollYD)<0.001); %index number where the eigenmotion starts
-range = (i_DrollYD-200:i_DrollYD+99);
-maxdr_DrollYD = max(dr(range));
-i_start_DrollYD = find(dr==maxdr_DrollYD);
+% i_DrollYD = find(abs(time-start_DrollYD)<0.001); %index number where the eigenmotion starts
+% range = (i_DrollYD-200:i_DrollYD+99);
+% maxdr_DrollYD = max(dr(range));
+% i_start_DrollYD = find(dr==maxdr_DrollYD);
 
-time_DrollYD = (1:t_DrollYD*10)/10;
-rollangle_DrollYD = rollangle(i_start_DrollYD:i_start_DrollYD+t_DrollYD*10-1);
-rollrate_DrollYD = rollrate(i_start_DrollYD:i_start_DrollYD+t_DrollYD*10-1);
-yawrate_DrollYD = yawrate(i_start_DrollYD:i_start_DrollYD+t_DrollYD*10-1);
+%time_DrollYD = (1:t_DrollYD*10)/10;
+rollangle_DrollYD = rollangle(i_start_DR_YD:i_start_DR_YD+length_DrollYD*10);
+rollrate_DrollYD = rollrate(i_start_DR_YD:i_start_DR_YD+length_DrollYD*10);
+yawrate_DrollYD = yawrate(i_start_DR_YD:i_start_DR_YD+length_DrollYD*10);
 
 % DrollYD1 = subplot(3,2,2);
 % plot(DrollYD1,time_DrollYD,rollangle_DrollYD);
@@ -185,17 +174,17 @@ yawrate_DrollYD = yawrate(i_start_DrollYD:i_start_DrollYD+t_DrollYD*10-1);
 
 % Aperiodic roll plots
 
-i_Aroll = find(abs(time-start_Aroll)<0.001); %index number where the eigenmotion starts
-range = (i_Aroll-200:i_Aroll+99);
-maxda_Aroll = max(da(range));
-i_start_Aroll = find(da==maxda_Aroll);
-% plot(delta_a(i_start_Aroll:i_start_Aroll+t_Aroll*10-1))
-% figure()
+% i_Aroll = find(abs(time-start_Aroll)<0.001); %index number where the eigenmotion starts
+% range = (i_Aroll-200:i_Aroll+99);
+% maxda_Aroll = max(da(range));
+% i_start_Aroll = find(da==maxda_Aroll);
+% % plot(delta_a(i_start_Aroll:i_start_Aroll+t_Aroll*10-1))
+% % figure()
 
-time_Aroll = (1:t_Aroll*10)/10;
-rollangle_Aroll = rollangle(i_start_Aroll:i_start_Aroll+t_Aroll*10-1);
-rollrate_Aroll = rollrate(i_start_Aroll:i_start_Aroll+t_Aroll*10-1);
-yawrate_Aroll = yawrate(i_start_Aroll:i_start_Aroll+t_Aroll*10-1);
+%time_Aroll = (1:t_Aroll*10)/10;
+rollangle_Aroll = rollangle(i_start_AR:i_start_AR+length_Aroll*10);
+rollrate_Aroll = rollrate(i_start_AR:i_start_AR+length_Aroll*10);
+yawrate_Aroll = yawrate(i_start_AR:i_start_AR+length_Aroll*10);
 
 % Aroll1 = subplot(3,1,1);
 % plot(Aroll1,time_Aroll,rollangle_Aroll);
@@ -219,15 +208,15 @@ yawrate_Aroll = yawrate(i_start_Aroll:i_start_Aroll+t_Aroll*10-1);
 
 % check start aileron input
 
-i_spiral = find(abs(time-start_spiral)<0.001); %index number where the eigenmotion starts
-range = (i_spiral-200:i_spiral+99);
-maxda_spiral = max(da(range));
-i_start_spiral = find(da==maxda_spiral);
+% i_spiral = find(abs(time-start_spiral)<0.001); %index number where the eigenmotion starts
+% range = (i_spiral-200:i_spiral+99);
+% maxda_spiral = max(da(range));
+% i_start_spiral = find(da==maxda_spiral);
 
-time_spiral = (1:t_spiral*10)/10;
-rollangle_spiral = rollangle(i_start_spiral:i_start_spiral+t_spiral*10-1);
-rollrate_spiral = rollrate(i_start_spiral:i_start_spiral+t_spiral*10-1);
-yawrate_spiral = yawrate(i_start_spiral:i_start_spiral+t_spiral*10-1);
+%time_spiral = (1:t_spiral*10)/10;
+rollangle_spiral = rollangle(i_start_SPI:i_start_SPI+length_spiral*10);
+rollrate_spiral = rollrate(i_start_SPI:i_start_SPI+length_spiral*10);
+yawrate_spiral = yawrate(i_start_SPI:i_start_SPI+length_spiral*10);
 
 % spiral1 = subplot(3,1,1);
 % plot(spiral1,time_spiral,rollangle_spiral);
